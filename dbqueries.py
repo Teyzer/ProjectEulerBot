5e772f9b88887cbab41e17fa3e5a3c876cdcadf2
preemptible <catmeowcat11@proton.me>
2023-10-16 23:18:44 +0300

    Update dbqueries.py
    I (believe) this is a neater & better implementation.

--- dbqueries.py
+++ dbqueries.py
@@ -1,108 +1,88 @@
 import pymysql
 import json
 import datetime
-
 import phone_api
-
 import pe_api
-
-credentials_server = {"user": "euler_remote", "db": "euler"}
-credentials_remote = credentials_server
 
 MYSQL_KEYFILE = "database_key.txt"
 
 DB_TOTAL_REQUESTS = 0
 DB_SESSION_REQUESTS = 0
 
-
-def setup_database_keys():
-
-    global credentials_server
-
-    # setup mysql key found in private file
+def setup_database_keys(credentials):
+    # Setup MySQL key found in a private file
     with open(MYSQL_KEYFILE) as f:
-        key = list(map(lambda x: x.replace("\n", "").replace("\r", ""), f.readlines()))
+        key = [line.strip() for line in f.readlines()]
 
-    credentials_server["password"] = key[0]
-    credentials_server["host"] = key[1]
+    credentials["password"] = key[0]
+    credentials["host"] = key[1]
 
-
-
-
-def single_req(_req, running_server=True):
-    con = open_con(running_server=running_server)
-    if con is False:
-        phone_api.bot_crashed("Error while querying database")
-        return {}
-    result = query(_req, con)
-    close_con(con)
-    return result
-
-
-def open_con(running_server=True):
+def open_connection(credentials):
     try:
-        if running_server:
-            return pymysql.connect(host=credentials_server["host"], user=credentials_server["user"], password=credentials_server["password"], db=credentials_server["db"])
-        else:
-            return pymysql.connect(host=credentials_remote["host"], user=credentials_remote["user"], password=credentials_remote["password"], db=credentials_remote["db"])
+        return pymysql.connect(
+            host=credentials["host"],
+            user=credentials["user"],
+            password=credentials["password"],
+            db=credentials["db"]
+        )
     except Exception as e:
-        print("Error while creating a connection to database:", e)
+        print("Error while creating a connection to the database:", e)
         phone_api.bot_crashed("Database request crash")
-        return False
+        return None
 
+def close_connection(connection):
+    if connection is not None:
+        connection.close()
 
-def query(_query, con):
+def execute_query(query, connection):
+    global DB_SESSION_REQUESTS, DB_TOTAL_REQUESTS
 
-    global DB_SESSION_REQUESTS, DB_TOTAL_REQUESTS 
-    
     DB_SESSION_REQUESTS += 1
     DB_TOTAL_REQUESTS += 1
-    
-    back = True
-    
-    if con is None or con is False:
-        phone_api.bot_crashed("Error while querying database")
-        return {}
-    
-    cur = con.cursor()
-    cur.execute(_query)
-    
-    if _query[:6] == "SELECT":
-        back = cur.fetchall()
-        field_names = [i[0] for i in cur.description]
-        back = to_json(back, field_names)
-    elif _query[:6] == "INSERT" or _query[:6] == "UPDATE":
-        con.commit()
-    
-    cur.close()
-    return back
 
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            if query.startswith("SELECT"):
+                result = cursor.fetchall()
+                field_names = [i[0] for i in cursor.description]
+                result = to_json(result, field_names)
+                return result
+            elif query.startswith("INSERT") or query.startswith("UPDATE"):
+                connection.commit()
+                return True
+    except Exception as e:
+        print("Error while executing the query:", e)
+        phone_api.bot_crashed("Error while querying the database")
+        return None
 
-def close_con(con):
-    if con is not None and con is not False:
-        con.close()
-
-
-def to_json(_object, headers):
+def to_json(result, headers):
     new_obj = {}
-    for index1, row in enumerate(_object):
+    for index1, row in enumerate(result):
         new_obj[index1] = {}
         for index2, column in enumerate(row):
-            dates_f = [isinstance(column, datetime.date), isinstance(column, datetime.datetime), isinstance(column, datetime.time)]
+            dates_f = [
+                isinstance(column, datetime.date),
+                isinstance(column, datetime.datetime),
+                isinstance(column, datetime.time)
+            ]
             if any(dates_f):
                 new_obj[index1][headers[index2]] = str(column)
             else:
                 new_obj[index1][headers[index2]] = column
     return new_obj
 
-
-def option_query(_query, connection=None):
-    if connection is None:
-        return single_req(_query)
-    else:
-        return query(_query, connection)
-
-
+def single_request(query, running_server=True):
+    connection = open_connection(credentials_server if running_server else credentials_remote)
+    if connection is not None:
+        result = execute_query(query, connection)
+        close_connection(connection)
+        return result
+    return {}
 
 if __name__ == "__main__":
-    print(single_req("SELECT * FROM friends", running_server=False))
+    credentials_server = {"user": "euler_remote", "db": "euler"}
+    credentials_remote = credentials_server.copy()
+    setup_database_keys(credentials_server)
+    print(single_request("SELECT * FROM friends", running_server=False))
+
