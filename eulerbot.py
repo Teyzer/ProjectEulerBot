c95e01553ba2669b3218bfc47422055f8c57f12b
Pacôme F. <pacomefromager@icloud.com>
2025-04-16 16:12:11 +0200

    setup function in eulerbot.py

--- eulerbot.py
+++ eulerbot.py
@@ -8,7 +8,11 @@ import json
 import sys
 
 
-if __name__ == '__main__':
+def setup() -> str:
+
+    """
+    Complete setup of the Bot, and returns the discord_key
+    """
 
     if len(sys.argv) < 2:
         raise Exception("Missing a profile filename.")
@@ -16,14 +20,22 @@ if __name__ == '__main__':
     with open(f"profiles/{sys.argv[1]}", "r") as f:
         profile = json.load(f)
 
-    temp_console = Console()
-    temp_console.log(f"[*] Started session with profile '{sys.argv[1]}'")
-
     pe_discord_api.pe_discord_api_setup(profile["announcement_channels"])
     pe_api.pe_api_setup(profile["session_keys"], profile["pe_account"])
     pe_database.database_setup(profile["database_file"])
+    
+    return profile["discord_key"]
+    
+
+if __name__ == '__main__':
+
+    temp_console = Console()
+    temp_console.log(f"[*] Started session with profile '{sys.argv[1]}'")
+
+    discord_key = setup()
     temp_console.rule()
-    pe_discord_api.bot.run(profile["discord_key"])
+    
+    pe_discord_api.bot.run(discord_key)
     
 
     
